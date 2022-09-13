33450c1789e9754e5612596dd8a06f9b8fc73f9d
Steyrix <steyrixio@gmail.com>
2022-09-13 15:17:44 +0300

    Slight code style fixes

--- app/src/main/java/com/example/currency_converter_mvi_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/currency_converter_mvi_compose/ui/theme/Theme.kt
@@ -28,7 +28,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CurrencyConverterMVIComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun CurrencyConverterMVIComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
