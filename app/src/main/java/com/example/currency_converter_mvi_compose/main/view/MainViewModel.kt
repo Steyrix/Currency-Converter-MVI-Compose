d0d768fbf45666dba0c26edff2c16173fba237c7
Steyrix <steyrixio@gmail.com>
2022-09-13 20:23:43 +0300

    Implement external layers' methods calling in viewmodel

--- app/src/main/java/com/example/currency_converter_mvi_compose/main/view/MainViewModel.kt
+++ app/src/main/java/com/example/currency_converter_mvi_compose/main/view/MainViewModel.kt
@@ -1,17 +1,84 @@
 package com.example.currency_converter_mvi_compose.main.view
 
+import androidx.lifecycle.viewModelScope
+import com.example.currency_converter_mvi_compose.main.data.CurrencyConverterRepository
+import com.example.currency_converter_mvi_compose.main.domain.ComputeCurrencyRatesUseCase
 import com.example.currency_converter_mvi_compose.view.BaseViewModel
+import kotlinx.coroutines.launch
 
-class MainViewModel : BaseViewModel<
+class MainViewModel(
+    private val repository: CurrencyConverterRepository,
+    private val currencyRatesComputer: ComputeCurrencyRatesUseCase
+) : BaseViewModel<
         MainScreenContract.State,
         MainScreenContract.Event,
         MainScreenContract.Effect>() {
 
+    init {
+        getAvailableCurrencies()
+        getCurrencyRates()
+    }
+
     override fun setInitialState(): MainScreenContract.State {
-        TODO("Not yet implemented")
+        return MainScreenContract.State(
+            currentCurrency = repository.getDefaultCurrency(),
+            currencies = emptyList(),
+            rates = emptyList(),
+            isLoading = true,
+            isError = false
+        )
     }
 
     override fun onEventReceived(event: MainScreenContract.Event) {
-        TODO("Not yet implemented")
+        when(event) {
+            is MainScreenContract.Event.AmountChanging -> {
+
+            }
+            is MainScreenContract.Event.CurrencySelection -> {
+
+            }
+        }
+    }
+
+    private fun getAvailableCurrencies() {
+        viewModelScope.launch {
+            repository.getAvailableCurrencies()
+                .onSuccess {
+                    setState {
+                        copy(
+                            currentCurrency = currencies.first(),
+                            currencies = it,
+                            isLoading = false
+                        )
+                    }
+                }
+                .onFailure {
+                    setState { copy(isError = true, isLoading = false) }
+                }
+        }
+    }
+
+    private fun getCurrencyRates() {
+        viewModelScope.launch {
+            repository.getAllCurrencyRates()
+                .onSuccess {
+                    getRatesForCurrency()
+                }
+                .onFailure {
+                    setState { copy(isError = true) }
+                }
+        }
+    }
+
+    private fun getRatesForCurrency() {
+        viewModelScope.launch {
+            currencyRatesComputer.getRatesForCurrency(getState().value.currentCurrency)
+                .onSuccess {
+                    setState { copy(rates = it) }
+                }
+                .onFailure {
+                    setState { copy(isError = true) }
+                }
+        }
     }
 }
