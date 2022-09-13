6f6751956f0fcf898d9531020e432daa39bd0888
Steyrix <steyrixio@gmail.com>
2022-09-13 15:18:17 +0300

    Add renamings in MainScreenContract.kt

--- app/src/main/java/com/example/currency_converter_mvi_compose/main/view/MainScreenContract.kt
+++ app/src/main/java/com/example/currency_converter_mvi_compose/main/view/MainScreenContract.kt
@@ -3,19 +3,24 @@ package com.example.currency_converter_mvi_compose.main.view
 import android.icu.util.Currency
 import com.example.currency_converter_mvi_compose.view.ViewEffect
 import com.example.currency_converter_mvi_compose.view.ViewEvent
+import com.example.currency_converter_mvi_compose.view.ViewState
 
 class MainScreenContract {
-    sealed class MainScreenEvent : ViewEvent {
-        data class AmountChanging(val newAmount: Float) : MainScreenEvent()
+    data class State(
+        val isLoading: Boolean = false
+    ) : ViewState
 
-        data class CurrencySelection(val newCurrency: Currency) : MainScreenEvent()
+    sealed class Event : ViewEvent {
+        data class AmountChanging(val newAmount: Float) : Event()
 
-        object ExchangeRateUpdating : MainScreenEvent()
+        data class CurrencySelection(val newCurrency: Currency) : Event()
+
+        object ExchangeRateUpdating : Event()
     }
 
-    sealed class MainScreenEffect : ViewEffect {
+    sealed class Effect : ViewEffect {
 
-        sealed class Update : MainScreenEffect() {
+        sealed class Update : Effect() {
             object Failed : Update()
 
             data class Succeeded(val newRate: Float) : Update()
