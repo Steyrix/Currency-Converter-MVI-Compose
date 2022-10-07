c2e9a47e112095a8a9318d5f888d9e06e2d5e55e
Steyrix <steyrixio@gmail.com>
2022-10-07 17:13:15 +0300

    Remove switching context to IO, since retrofit does it automatically

--- app/src/main/java/com/example/currency_converter_mvi_compose/main/data/repository/SafeApiCallRepository.kt
+++ app/src/main/java/com/example/currency_converter_mvi_compose/main/data/repository/SafeApiCallRepository.kt
@@ -1,15 +1,11 @@
 package com.example.currency_converter_mvi_compose.main.data.repository
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 interface SafeApiCallRepository {
     suspend fun <T : Any> apiCall(
         call: suspend () -> Response<T>
     ): Result<T> = runCatching {
-        withContext(Dispatchers.IO) {
             call.invoke().body() ?: throw Exception()
-        }
     }
 }
