15c0c09e1b7883a32957ad99533f09d38e987228
Steyrix <steyrixio@gmail.com>
2022-11-04 22:11:14 +0100

    Revert "Remove switching context to IO, since retrofit does it automatically"
    This reverts commit c2e9a47e112095a8a9318d5f888d9e06e2d5e55e.

--- app/src/main/java/com/example/currency_converter_mvi_compose/main/data/repository/SafeApiCallRepository.kt
+++ app/src/main/java/com/example/currency_converter_mvi_compose/main/data/repository/SafeApiCallRepository.kt
@@ -1,11 +1,15 @@
 package com.example.currency_converter_mvi_compose.main.data.repository
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 interface SafeApiCallRepository {
     suspend fun <T : Any> apiCall(
         call: suspend () -> Response<T>
     ): Result<T> = runCatching {
+        withContext(Dispatchers.IO) {
             call.invoke().body() ?: throw Exception()
+        }
     }
 }
